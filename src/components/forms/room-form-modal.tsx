3827b766a467bd4c3dd1073ace9a35fc9e4a949d
HRN-BK <huy.nguyenbk218cnsh@hcmut.edu.vn>
2025-09-04 14:18:52 +0700

    clean: remove unnecessary comment from room form modal

--- src/components/forms/room-form-modal.tsx
+++ src/components/forms/room-form-modal.tsx
@@ -254,7 +254,6 @@ const onSubmit = async (data: RoomFormData) => {
                 />
               </div>
 
-              /* Area and Rent - Mobile friendly grid */
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 <FormField
                   control={form.control}
